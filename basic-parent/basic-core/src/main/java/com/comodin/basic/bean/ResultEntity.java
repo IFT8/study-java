ac559ee39ebbe67ba035c32cebda8a4ed0e74c09
assupg <assupg@hotmail.com>
2017-08-08 22:55:39 +0800

    temp

--- basic-parent/basic-core/src/main/java/com/comodin/basic/bean/ResultEntity.java
+++ basic-parent/basic-core/src/main/java/com/comodin/basic/bean/ResultEntity.java
@@ -3,31 +3,47 @@ package com.comodin.basic.bean;
 @SuppressWarnings("unused")
 public class ResultEntity {
     private String resultCode;
-    private Object resultMsg;
+    private String resultMsg;
+    private Object resultData;
 
     public ResultEntity() {
     }
 
-    public ResultEntity(String resultCode, Object resultMsg) {
+    public ResultEntity(String resultCode, Object resultData) {
+        this.resultCode = resultCode;
+        this.resultData = resultData;
+    }
+
+    public ResultEntity(String resultCode, String resultMsg, Object resultData) {
         this.resultCode = resultCode;
         this.resultMsg = resultMsg;
+        this.resultData = resultData;
     }
 
     public String getResultCode() {
-        return resultCode;
+        return (resultCode == null) ? null : resultCode.trim();
     }
 
     public ResultEntity setResultCode(String resultCode) {
-        this.resultCode = resultCode;
+        this.resultCode = (resultCode == null) ? null : resultCode.trim();
         return this;
     }
 
-    public Object getResultMsg() {
-        return resultMsg;
+    public String getResultMsg() {
+        return (resultMsg == null) ? null : resultMsg.trim();
     }
 
-    public ResultEntity setResultMsg(Object resultMsg) {
-        this.resultMsg = resultMsg;
+    public ResultEntity setResultMsg(String resultMsg) {
+        this.resultMsg = (resultMsg == null) ? null : resultMsg.trim();
+        return this;
+    }
+
+    public Object getResultData() {
+        return resultData;
+    }
+
+    public ResultEntity setResultData(Object resultData) {
+        this.resultData = resultData;
         return this;
     }
 }
