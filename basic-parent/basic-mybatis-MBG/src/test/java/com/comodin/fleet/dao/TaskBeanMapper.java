9314b4cca0a970c1af5db788512b6ca957110a41
assupg <assupg@hotmail.com>
2017-08-18 12:39:05 +0800

    temp

--- basic-parent/basic-mybatis-MBG/src/test/java/com/comodin/fleet/dao/TaskBeanMapper.java
+++
@@ -1,7 +0,0 @@
-package com.comodin.fleet.dao;
-
-import com.comodin.basic.dao.IBaseDao;
-import com.comodin.fleet.bean.TaskBean;
-
-public interface TaskBeanMapper extends IBaseDao<TaskBean> {
-}
--- basic-parent/basic-mybatis-MBG/src/test/java/com/comodin/fleet/service/ITaskBeanService.java
+++
@@ -1,9 +0,0 @@
-package com.comodin.fleet.service;
-
-import com.comodin.basic.service.IBaseService;
-import com.comodin.basic.vo.BaseVo;
-import com.comodin.fleet.core.bean.TaskBean;
-
-public interface ITaskBeanService extends IBaseService<TaskBean, BaseVo<TaskBean>> {
-
-}
