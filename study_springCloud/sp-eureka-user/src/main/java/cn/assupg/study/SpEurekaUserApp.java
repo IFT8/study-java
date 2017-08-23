22c4984a7107d444a0b399a0e10ea755f38f6504
assupg <assupg@hotmail.com>
2017-08-24 00:49:28 +0800

    temp

---
+++ study_springCloud/sp-eureka-user/src/main/java/cn/assupg/study/SpEurekaUserApp.java
@@ -0,0 +1,12 @@
+package cn.assupg.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpEurekaUserApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpEurekaUserApp.class, args);
+	}
+}
---
+++ study_springCloud/sp-eureka-user/src/main/java/cn/assupg/study/repository/IUserRepository.java
@@ -0,0 +1,10 @@
+package cn.assupg.study.repository;
+
+import cn.assupg.study.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRepository extends JpaRepository<User, Long> {
+
+}
