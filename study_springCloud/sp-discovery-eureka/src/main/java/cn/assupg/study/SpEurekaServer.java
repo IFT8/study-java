3b582e8ecd6138314dcda5210487cbf01573b3f1
assupg <assupg@hotmail.com>
2017-08-24 02:36:18 +0800

    temp

---
+++ study_springCloud/sp-discovery-eureka/src/main/java/cn/assupg/study/SpEurekaServer.java
@@ -0,0 +1,14 @@
+package cn.assupg.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpEurekaServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpEurekaServer.class, args);
+    }
+}
--- study_springCloud/sp-eureka-user/src/main/java/cn/assupg/study/SpEurekaUserApp.java
+++ study_springCloud/sp-eureka-user/src/main/java/cn/assupg/study/SpEurekaUserApp.java
@@ -2,8 +2,10 @@ package cn.assupg.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpEurekaUserApp {
 
 	public static void main(String[] args) {
