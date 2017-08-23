0083aa53daf47f93c32d5ccc69acfb5a8e022186
assupg <assupg@hotmail.com>
2017-08-24 00:25:15 +0800

    temp

---
+++ study_springCloud/sp-movie/src/main/java/cn/assupg/study/controller/MovieController.java
@@ -0,0 +1,26 @@
+package cn.assupg.study.controller;
+
+import cn.assupg.study.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class MovieController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${user.userServicePath}")
+    private String userServicePath;
+
+    @GetMapping("/movie/{id}")
+    public User findById(@PathVariable Long id) {
+        //String url = "http://localhost:7900/user/" + id;
+        String url = this.userServicePath + id;
+        return this.restTemplate.getForObject(url, User.class);
+    }
+}
---
+++ study_springCloud/sp-user/src/main/java/cn/assupg/study/SpUserApp.java
@@ -0,0 +1,12 @@
+package cn.assupg.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpUserApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpUserApp.class, args);
+	}
+}
