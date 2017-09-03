2cd3a9c0cf8daac7bc1c0175c233e1fda462d2a5
assupg <assupg@hotmail.com>
2017-09-03 11:28:10 +0800

    temp

--- basic-parent/basic-demo/src/main/java/com/comodin/fleet/constants/i18n/ClientBeanI18nConstant.java
+++ basic-parent/basic-demo/src/main/java/com/comodin/fleet/constants/i18n/ClientBeanI18nConstant.java
@@ -4,81 +4,49 @@ package com.comodin.fleet.constants.i18n;
 public class ClientBeanI18nConstant  {
 
     /**
-     * 客户类型为[CLIENT]所属哪一个CIT的客户，与t_client.client_id关联，且类型为[CIT]；0为CIT
+     * 客户内部编号
      */
-    public static final String CLIENT_BEAN_CIT_ID_LENGTH = "CLIENT_BEAN_CIT_ID_LENGTH";
-    /**
-     * 客户名称
-     */
-    public static final String CLIENT_BEAN_NAME_LENGTH = "CLIENT_BEAN_NAME_LENGTH";
-    /**
-     * 客户类型【CIT,CLIENT...】
-     */
-    public static final String CLIENT_BEAN_TYPE_LENGTH = "CLIENT_BEAN_TYPE_LENGTH";
+    public static final String CLIENT_BEAN_INTERNAL_ID_NOT_BLANK = "CLIENT_BEAN_INTERNAL_ID_NOT_BLANK";
     /**
      * 客户联系电话
      */
     public static final String CLIENT_BEAN_TELE_PHONE_LENGTH = "CLIENT_BEAN_TELE_PHONE_LENGTH";
     /**
-     * 创建时间
-     */
-    public static final String CLIENT_BEAN_CREATE_TIMESTAMP_DATE_TIME_FORMAT = "CLIENT_BEAN_CREATE_TIMESTAMP_DATE_TIME_FORMAT";
-    /**
-     * 逻辑删除标志【N[正常]，Y[删除]】
-     */
-    public static final String CLIENT_BEAN_DELETE_FLAG_LENGTH = "CLIENT_BEAN_DELETE_FLAG_LENGTH";
-    /**
-     * 客户内部编号
-     */
-    public static final String CLIENT_BEAN_INTERNAL_ID_LENGTH = "CLIENT_BEAN_INTERNAL_ID_LENGTH";
-    /**
      * 客户地址经度
      */
     public static final String CLIENT_BEAN_LONGITUDE_LENGTH = "CLIENT_BEAN_LONGITUDE_LENGTH";
     /**
-     * 客户地址纬度
+     * 客户类型为[CLIENT]所属哪一个CIT的客户，与t_client.client_id关联，且类型为[CIT]；0为CIT
      */
-    public static final String CLIENT_BEAN_LATITUDE_LENGTH = "CLIENT_BEAN_LATITUDE_LENGTH";
-    /**
-     * 创建时间
-     */
-    public static final String CLIENT_BEAN_CREATE_TIMESTAMP_NOT_NULL = "CLIENT_BEAN_CREATE_TIMESTAMP_NOT_NULL";
+    public static final String CLIENT_BEAN_CIT_ID_LENGTH = "CLIENT_BEAN_CIT_ID_LENGTH";
     /**
      * 客户联系电话
      */
     public static final String CLIENT_BEAN_PHONE_LENGTH = "CLIENT_BEAN_PHONE_LENGTH";
     /**
-     * 客户内部编号
-     */
-    public static final String CLIENT_BEAN_INTERNAL_ID_NOT_BLANK = "CLIENT_BEAN_INTERNAL_ID_NOT_BLANK";
-    /**
-     * 创建人,与t_crew.crew_username字段关联
-     */
-    public static final String CLIENT_BEAN_CREATE_BY_LENGTH = "CLIENT_BEAN_CREATE_BY_LENGTH";
-    /**
      * 数据库主键ID
      */
-    public static final String CLIENT_BEAN_ID_LENGTH = "CLIENT_BEAN_ID_LENGTH";
-    /**
-     * 创建人,与t_crew.crew_username字段关联
-     */
-    public static final String CLIENT_BEAN_CREATE_BY_NOT_BLANK = "CLIENT_BEAN_CREATE_BY_NOT_BLANK";
-    /**
-     * 逻辑删除标志【N[正常]，Y[删除]】
-     */
-    public static final String CLIENT_BEAN_DELETE_FLAG_NOT_BLANK = "CLIENT_BEAN_DELETE_FLAG_NOT_BLANK";
-    /**
-     * 客户名称
-     */
-    public static final String CLIENT_BEAN_NAME_NOT_BLANK = "CLIENT_BEAN_NAME_NOT_BLANK";
+    public static final String CLIENT_BEAN_ID_NOT_NULL = "CLIENT_BEAN_ID_NOT_NULL";
     /**
      * 客户所属领域,retailer,bank...
      */
     public static final String CLIENT_BEAN_SECTOR_LENGTH = "CLIENT_BEAN_SECTOR_LENGTH";
     /**
-     * 客户类型为[CLIENT]所属哪一个CIT的客户，与t_client.client_id关联，且类型为[CIT]；0为CIT
+     * 客户名称
      */
-    public static final String CLIENT_BEAN_CIT_ID_NOT_NULL = "CLIENT_BEAN_CIT_ID_NOT_NULL";
+    public static final String CLIENT_BEAN_NAME_NOT_BLANK = "CLIENT_BEAN_NAME_NOT_BLANK";
+    /**
+     * 逻辑删除标志【N[正常]，Y[删除]】
+     */
+    public static final String CLIENT_BEAN_DELETE_FLAG_NOT_BLANK = "CLIENT_BEAN_DELETE_FLAG_NOT_BLANK";
+    /**
+     * 创建时间
+     */
+    public static final String CLIENT_BEAN_CREATE_TIMESTAMP_DATE_TIME_FORMAT = "CLIENT_BEAN_CREATE_TIMESTAMP_DATE_TIME_FORMAT";
+    /**
+     * 客户类型【CIT,CLIENT...】
+     */
+    public static final String CLIENT_BEAN_TYPE_NOT_BLANK = "CLIENT_BEAN_TYPE_NOT_BLANK";
     /**
      * 客户总部地址
      */
@@ -86,10 +54,42 @@ public class ClientBeanI18nConstant  {
     /**
      * 数据库主键ID
      */
-    public static final String CLIENT_BEAN_ID_NOT_NULL = "CLIENT_BEAN_ID_NOT_NULL";
+    public static final String CLIENT_BEAN_ID_LENGTH = "CLIENT_BEAN_ID_LENGTH";
+    /**
+     * 客户名称
+     */
+    public static final String CLIENT_BEAN_NAME_LENGTH = "CLIENT_BEAN_NAME_LENGTH";
+    /**
+     * 客户内部编号
+     */
+    public static final String CLIENT_BEAN_INTERNAL_ID_LENGTH = "CLIENT_BEAN_INTERNAL_ID_LENGTH";
+    /**
+     * 创建时间
+     */
+    public static final String CLIENT_BEAN_CREATE_TIMESTAMP_NOT_NULL = "CLIENT_BEAN_CREATE_TIMESTAMP_NOT_NULL";
+    /**
+     * 客户类型为[CLIENT]所属哪一个CIT的客户，与t_client.client_id关联，且类型为[CIT]；0为CIT
+     */
+    public static final String CLIENT_BEAN_CIT_ID_NOT_NULL = "CLIENT_BEAN_CIT_ID_NOT_NULL";
+    /**
+     * 客户地址纬度
+     */
+    public static final String CLIENT_BEAN_LATITUDE_LENGTH = "CLIENT_BEAN_LATITUDE_LENGTH";
+    /**
+     * 创建人,与t_crew.crew_username字段关联
+     */
+    public static final String CLIENT_BEAN_CREATE_BY_NOT_BLANK = "CLIENT_BEAN_CREATE_BY_NOT_BLANK";
+    /**
+     * 创建人,与t_crew.crew_username字段关联
+     */
+    public static final String CLIENT_BEAN_CREATE_BY_LENGTH = "CLIENT_BEAN_CREATE_BY_LENGTH";
+    /**
+     * 逻辑删除标志【N[正常]，Y[删除]】
+     */
+    public static final String CLIENT_BEAN_DELETE_FLAG_LENGTH = "CLIENT_BEAN_DELETE_FLAG_LENGTH";
     /**
      * 客户类型【CIT,CLIENT...】
      */
-    public static final String CLIENT_BEAN_TYPE_NOT_BLANK = "CLIENT_BEAN_TYPE_NOT_BLANK";
+    public static final String CLIENT_BEAN_TYPE_LENGTH = "CLIENT_BEAN_TYPE_LENGTH";
 
 }
